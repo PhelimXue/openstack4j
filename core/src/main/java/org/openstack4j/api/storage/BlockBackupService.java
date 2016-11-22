b7e188248539a0518ee1992a75b40bb152f97e44
PhelimXue <polka.xue@gmail.com>
2016-11-22 15:59:29 +0800

    新增 backup delete 功能

--- core/src/main/java/org/openstack4j/api/storage/BlockBackupService.java
+++ core/src/main/java/org/openstack4j/api/storage/BlockBackupService.java
@@ -3,10 +3,12 @@ package org.openstack4j.api.storage;
 import java.util.List;
 
 import org.openstack4j.common.RestService;
+import org.openstack4j.model.common.ActionResponse;
 import org.openstack4j.model.storage.block.backup.Backup;
 
 public interface BlockBackupService extends RestService{
 
 	List<? extends Backup> list();
 	Backup get(String backupId);
+	ActionResponse delete(String backupId);
 }
--- core/src/main/java/org/openstack4j/openstack/storage/block/internal/BlockBackupServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/storage/block/internal/BlockBackupServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org.openstack4j.api.storage.BlockBackupService;
+import org.openstack4j.model.common.ActionResponse;
 import org.openstack4j.model.storage.block.backup.Backup;
 import org.openstack4j.openstack.storage.block.domain.CinderVolumeBackup;
 import org.openstack4j.openstack.storage.block.domain.CinderVolumeBackup.Backups;
@@ -20,6 +21,12 @@ public class BlockBackupServiceImpl extends BaseBlockStorageServices implements 
 	public Backup get(String backupId) {
 		checkNotNull(backupId);
 		return get(CinderVolumeBackup.class, uri("/backups/%s", backupId)).execute();
+	}
+
+	@Override
+	public ActionResponse delete(String backupId) {
+		checkNotNull(backupId);
+        return deleteWithResponse(uri("/backups/%s", backupId)).execute();
 	}
 
 }
